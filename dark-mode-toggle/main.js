6374f9d9364f2aae11596ad1b262da4041d61884
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-03-20 22:35:16 +0530

    Create TODO list structure and done with styling

--- dark-mode-toggle/main.js
+++ dark-mode-toggle/main.js
@@ -2,12 +2,9 @@ const toggleBtn = document.getElementById("toggleButton");
 
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  //   console.log(document.body.classList);
   if (document.body.classList.value == "dark") {
     toggleBtn.innerText = "Toggle Light Mode";
   } else {
     toggleBtn.innerText = "Toggle Dark Mode";
   }
-  //   console.log("yes");
 });
-// console.log(document.body.classList);
